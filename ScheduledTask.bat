08c2f8dc3fcb1f0075ebbac99941a95eb7bc589c
KThompsonBID <115190595+KThompsonBID@users.noreply.github.com>
2023-01-24 11:45:36 -0700

    Update ScheduledTask.bat

--- ScheduledTask.bat
+++ ScheduledTask.bat
@@ -1,2 +1,2 @@
-schtasks /create /sc ONLOGON /tn "VPN Security Lockout - logon" /tr "C:\Hideproxycheck - Logon.vbs"
-schtasks /create /sc minute /mo 1 /tn "VPN Security Lockout" /tr "C:\Hideproxycheck.vbs"
+schtasks /create /sc ONLOGON /tn "VPN Security Lockout - logon" /tr "C:\Scripts\Hideproxycheck - Logon.vbs"
+schtasks /create /sc minute /mo 1 /tn "VPN Security Lockout" /tr "C:\Scripts\Hideproxycheck.vbs"
