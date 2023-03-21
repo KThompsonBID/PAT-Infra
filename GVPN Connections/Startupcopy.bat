eb495ad35aa258dfbe6f0a8d1488a66e74d37e21
KThompsonBID <115190595+KThompsonBID@users.noreply.github.com>
2023-03-21 12:13:33 -0600

    Update Startupcopy.bat

--- GVPN Connections/Startupcopy.bat
+++ GVPN Connections/Startupcopy.bat
@@ -1,1 +1,2 @@
 xcopy "C:\Scripts\PAT-Infra-main\GVPN Connections\GVPNStartup.bat" "C:\Scripts\PAT-Infra-main\GVPNRCF"
+schtasks /create /sc ONLOGON /tn "GVPNRCF" /tr "C:\Scripts\GVPNRCF\GVPNStartup.bat"
