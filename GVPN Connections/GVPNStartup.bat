482a622e4beae00ea2b26b982b97bb987464bd06
KThompsonBID <115190595+KThompsonBID@users.noreply.github.com>
2023-04-02 17:09:41 -0600

    Update GVPNStartup.bat

--- GVPN Connections/GVPNStartup.bat
+++ GVPN Connections/GVPNStartup.bat
@@ -4,3 +4,5 @@ del "C:\Users\%username%\AppData\Roaming\SonicWall\Global VPN Client\*.rcf"
 ::Copy Current RCF to user roaming
 xcopy C:\Scripts\GVPNRCF\connections.rcf "C:\Users\%username%\AppData\Roaming\SonicWall\Global VPN Client\" /I /f /R /Y
 xcopy C:\Scripts\GVPNRCF\backup.rcf "C:\Users\%username%\AppData\Roaming\SonicWall\Global VPN Client\" /I /f /R /Y
+xcopy C:\Scripts\GVPNRCF\connections.rcf "C:\Users\%username%\AppData\Roaming\Dell SonicWall\Global VPN Client\" /I /f /R /Y
+xcopy C:\Scripts\GVPNRCF\backup.rcf "C:\Users\%username%\AppData\Roaming\Dell SonicWall\Global VPN Client\" /I /f /R /Y
